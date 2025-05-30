6d20674161db22b79131d03787e4339e14ebdf9e
WegenerSteven <wegenersteven580@gmail.com>
2025-05-30 11:47:19 +0300

    configure TypeOrm, initialize app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const PORT: number = parseInt(configService.getOrThrow<string>('PORT'), 10);
+
+  await app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`connected to Database`);
+  });
 }
 bootstrap();
